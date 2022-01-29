d79a8c44c3f3a5cb1d0de15d6262aea0cfe32e96
Gabrielish <gabriel_bundea@hotmail.com>
2022-01-29 23:11:23 +0200

    Implementing the Again button

--- script.js
+++ script.js
@@ -8,8 +8,7 @@ document.querySelector('.score').textContent = 10
 document.querySelector('.guess').value = 20
 */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1
-document.querySelector('.number').textContent = secretNumber
+let secretNumber = Math.trunc(Math.random() * 20) + 1
 
 let score = 20
 
@@ -25,6 +24,7 @@ document.querySelector('.check').addEventListener('click', function(){
     // When player wins
     } else if (guess == secretNumber) {
         document.querySelector('.message').textContent = 'Correct Number!🎉'
+        document.querySelector('.number').textContent = secretNumber
         document.querySelector('body').style.backgroundColor = '#60b347'
         document.querySelector('.number').style.width = '30rem'
     // When guess is too high
@@ -50,4 +50,15 @@ document.querySelector('.check').addEventListener('click', function(){
         }
         
     }
+})
+
+document.querySelector('.again').addEventListener('click', function (){
+    score = 20
+    secretNumber = Math.trunc(Math.random() * 20) + 1
+    document.querySelector('.message').textContent = 'Start guessing...'
+    document.querySelector('.score').textContent = score
+    document.querySelector('.number').textContent = '?'
+    document.querySelector('.guess').value = ''
+    document.querySelector('body').style.backgroundColor = '#222'
+    document.querySelector('.number').style.width = '15rem'
 })
