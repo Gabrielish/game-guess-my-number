334630d414d742fccf613604688e011632993af8
Gabrielish <gabriel_bundea@hotmail.com>
2022-01-29 23:20:18 +0200

    Implementing Highscores

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ document.querySelector('.guess').value = 20
 let secretNumber = Math.trunc(Math.random() * 20) + 1
 
 let score = 20
+let highscore = 0
 
 document.querySelector('.check').addEventListener('click', function(){
     const guess = Number(document.querySelector('.guess').value)
@@ -27,6 +28,12 @@ document.querySelector('.check').addEventListener('click', function(){
         document.querySelector('.number').textContent = secretNumber
         document.querySelector('body').style.backgroundColor = '#60b347'
         document.querySelector('.number').style.width = '30rem'
+
+        if(score > highscore) {
+            highscore = score
+            document.querySelector('.highscore').textContent = highscore
+        }
+
     // When guess is too high
     } else if (guess > secretNumber) {
         if(score > 1) {
