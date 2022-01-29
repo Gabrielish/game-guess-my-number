99137052865b2471ba0490d8ec613432517bc2f2
Gabrielish <gabriel_bundea@hotmail.com>
2022-01-29 22:09:19 +0200

    Handling Click Events

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 'use strict';
 
-console.log(document.querySelector('.message'))
+/* console.log(document.querySelector('.message'))
 document.querySelector('.message').textContent = 'Correct Number!🎉'
 document.querySelector('.number').textContent = 13
 document.querySelector('.score').textContent = 10
 
 document.querySelector('.guess').value = 20
+*/
+
+document.querySelector('.check').addEventListener('click', function(){
+    const guess = Number(document.querySelector('.guess').value)
+    console.log(typeof guess, guess)
+
+    if(!guess) {
+        document.querySelector('.message').textContent = 'No number ⛔'
+    }
+})
